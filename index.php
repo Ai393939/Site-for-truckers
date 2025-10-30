05e18010fd618f0e7f5eaedbd9e8941e7616d109
Vishnyak Elizaveta <lizavisnak@gmail.com>
2025-10-30 10:15:00 +0300

    Маршрутизатор и новые стили

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$path = trim($path, '/');
+
+if (empty($path)) {
+    include 'MainPage.html';
+    exit;
+}
+
+switch ($path) {
+    case 'submit-request':
+        include 'SubmitRequest.html';
+        break;
+    case 'feedback':
+        include 'Feedback.html';
+        break;
+    case 'search':
+        if (isset($_GET['query'])) {
+            $search_query = htmlspecialchars($_GET['query']);
+            echo "<h1>Результаты поиска: $search_query</h1>";
+        }
+        break;
+    default:
+        http_response_code(404);
+        echo "<h1>Страница не найдена</h1>";
+        echo "<p>Запрошенный адрес: /$path</p>";
+        break;
+}
+?>
